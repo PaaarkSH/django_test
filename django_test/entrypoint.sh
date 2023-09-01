b3c2b71657b02c5905080fed6c4e2e3d7b8a7d0d
박상훈 <psh950@naver.com>
2023-09-01 15:42:20 +0900

    gunicorn 설정

--- django_test/entrypoint.sh
+++ django_test/entrypoint.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# db 사용 가능까지 기리는 스크립트 코드
-echo "db 사용을 기다리는 중입니다"
-./wait-for-it.sh db:5432 -t 120
+python manage.py makemigrations
+python manage.py migrate
 
-WAIT_FOR_IT_RESULT=$?
-if [[ $WAIT_FOR_IT_RESULT -eq 0 ]]; then
-    echo "db 사용이 가능합니다"
-    python manage.py makemigrations
-    python manage.py migrate
-    gunicorn base.wsgi:application --bind 0.0.0.0:8000
-else
-    echo "db 사용 대기 중에 문제가 발생하였습니다."
-fi
+gunicorn base.wsgi:application --bind 0.0.0.0:8000 --workers 4
+
